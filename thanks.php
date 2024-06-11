b1210159fb37e8ae071883793c3850f153fd3c7d
kamiliin <ka.robledo@duocuc.cl>
2024-06-11 16:02:56 -0400

    footer pagina de agradecimiento

--- thanks.php
+++ thanks.php
@@ -82,6 +82,51 @@ header("refresh: 8; url=index.php");
         </div>
     </div>
 
+    <footer class="bg-dark text-white ">
+    <div class="container py-5">
+      <div class="row">
+        <div class="col-md-4 mb-3 text-center">
+          <img src="https://solarysustentablechile.cl/img/logofooter.png" alt="Logo de la Empresa" class="img-fluid mb-3" style="width:120px">
+          <p>Somos una empresa dedicada a ofrecer soluciones de energía solar eficientes y sostenibles para hogares y empresas.</p>
+        </div>
+        <div class="col-md-4 mb-3 pt-3 ps-lg-5">
+          <h5 >SERVICIOS</h5>
+          <ul class="list-unstyled">
+            <li><a href="#" class="text-white fs-6">Kit Solares</a></li>
+            <li><a href="#" class="text-white">Energía solar</a></li>
+            <li><a href="#" class="text-white">Hogar</a></li>
+            <li><a href="#" class="text-white">Energía solar empresas</a></li>
+            <li><a href="#" class="text-white">Servicio técnico</a></li>
+
+          </ul>
+        </div>
+        <div class="col-md-4 pt-3 mb-3">
+          <h5>NOSOTROS</h5>
+          <ul class="list-unstyled">
+            <li><i class="fas h4 fa-map-marker-alt"></i> Dirección: Valparaiso, Viña Del Mar Chile            </li>
+            <li><i class="fas h4 fa-phone"></i> Teléfono: +569 51906473            </li>
+            <li><i class="fas h4 fa-envelope"></i> Email: solarysustentable.91@gmail.com            </li>
+          </ul>
+          <div class="mt-3">
+            <a href="#" class="text-white mr-3"><i class="fab fa-facebook-f"></i></a>
+            <a href="#" class="text-white mr-3"><i class="fab fa-instagram"></i></a>
+            <a href="#" class="text-white"><i class="fab fa-whatsapp"></i></a>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12 text-center">
+          <p>&copy; 2024 solarysustentablechile. Todos los derechos reservados.</p>
+          <div class="d-flex justify-content-center mt-3">
+            <img src="https://via.placeholder.com/150" alt="Facebook" class="mx-2" style="width: 30px; height: 30px;">
+            <img src="https://via.placeholder.com/150" alt="Instagram" class="mx-2" style="width: 30px; height: 30px;">
+            <img src="https://via.placeholder.com/150" alt="WhatsApp" class="mx-2" style="width: 30px; height: 30px;">
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
 
 
 

d6f1ed324adc58f56a3119d23cf660a5b8c1a870
kamiliin <ka.robledo@duocuc.cl>
2024-10-29 15:23:44 -0300

    update

--- kit-solares.php
+++ kit-solares.php
@@ -11,6 +11,7 @@
     <link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
     <script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="icon" href="/img/logofooter.png">
 
 
     <link rel="stylesheet" href="/style.css">
